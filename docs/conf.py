57e57b20d44fbe25ce36de340362beecf0628824
shijl0925 <18221510672@139.com>
2022-04-10 22:59:29 +0800

    bug fix: mask password in Python logs

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Jialiang Shi'
 author = 'Jialiang Shi'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.7'
+release = '1.2.8'
 
 
 # -- General configuration ---------------------------------------------------
