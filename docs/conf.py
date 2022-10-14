5b2c5c8c7876fa8cb660b3c9ac1a3997e4e94737
shijl0925 <18221510672@139.com>
2022-10-14 21:32:51 +0800

    update version to 1.3.4

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Jialiang Shi'
 author = 'Jialiang Shi'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.3'
+release = '1.3.4'
 
 
 # -- General configuration ---------------------------------------------------
