2b4c59594e5e4288677635fbcb0cb5c88a0dc8e8
shijl0925 <18221510672@139.com>
2023-05-19 11:20:53 +0800

    Update users.py

--- sonarqube/cloud/users.py
+++ sonarqube/cloud/users.py
@@ -1,43 +1,15 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # @Author: Jialiang Shi
-from sonarqube.utils.rest_client import RestClient
-from sonarqube.utils.config import API_USERS_SEARCH_ENDPOINT, API_USERS_GROUPS_ENDPOINT
+from sonarqube.community.users import SonarQubeBaseUsers
+from sonarqube.utils.config import API_USERS_GROUPS_ENDPOINT
 from sonarqube.utils.common import GET
 
 
-class SonarCloudUsers(RestClient):
+class SonarCloudUsers(SonarQubeBaseUsers):
     """
     SonarCloud users Operations
     """
-
-    MAX_SEARCH_NUM = 200
-
-    def __init__(self, **kwargs):
-        """
-
-        :param kwargs:
-        """
-        super(SonarCloudUsers, self).__init__(**kwargs)
-
-    def get(self, login):
-        result = self.search_users(q=login)
-        users = result.get("users", [])
-
-        for user in users:
-            if user["login"] == login:
-                return user
-
-    @GET(API_USERS_SEARCH_ENDPOINT)
-    def search_users(self, q=None, p=None, ps=None):
-        """
-        Get a list of active users.
-
-        :param q: Filter on login, name and email
-        :param p: page number.
-        :param ps: Page size. Must be greater than 0 and less or equal than 500
-        :return:
-        """
 
     @GET(API_USERS_GROUPS_ENDPOINT)
     def search_groups_user_belongs_to(
