b9b3a71f1c83fdd0e0ba9a47e763665153babc49
shijl0925 <18221510672@139.com>
2023-05-19 11:18:55 +0800

    update package version

--- sonarqube/__init__.py
+++ sonarqube/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # @Author: Jialiang Shi
-__version__ = "1.3.7"
+__version__ = "1.3.8"
 
 from sonarqube.community import SonarQubeClient
 from sonarqube.cloud import SonarCloudClient
