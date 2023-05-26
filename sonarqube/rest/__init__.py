5d75667c222969b21123b1468dec8d1c9639f407
shijl0925 <1213970695@qq.com>
2023-05-26 22:35:27 +0800

    update reset apis

--- sonarqube/rest/__init__.py
+++ sonarqube/rest/__init__.py
@@ -11,43 +11,27 @@ from sonarqube.rest.projects import SonarQubeProjects
 from sonarqube.rest.user_groups import SonarQubeUserGroups
 from sonarqube.rest.issues import SonarQubeIssues
 from sonarqube.rest.measures import SonarQubeMeasures
-from sonarqube.rest.notifications import SonarQubeNotifications
-from sonarqube.rest.project_links import SonarQubeProjectLinks
-from sonarqube.rest.permissions import SonarQubePermissions
 from sonarqube.rest.ce import SonarQubeCe
 from sonarqube.rest.project_branches import SonarQubeProjectBranches
 from sonarqube.rest.qualitygates import SonarQubeQualityGates
-from sonarqube.rest.components import SonarQubeComponents
 from sonarqube.rest.rules import SonarQubeRules
 from sonarqube.rest.qualityprofiles import SonarQubeQualityProfiles
 from sonarqube.rest.duplications import SonarQubeDuplications
 from sonarqube.rest.metrics import SonarQubeMetrics
-from sonarqube.rest.hotspots import SonarQubeHotspots
-from sonarqube.rest.new_code_periods import SonarQubeNewcodeperiods
 from sonarqube.rest.settings import SonarQubeSettings
-from sonarqube.rest.sources import SonarQubeSources
 from sonarqube.rest.auth import SonarQubeAuth
 from sonarqube.rest.favorites import SonarQubeFavorites
 from sonarqube.rest.languages import SonarQubeLanguages
-from sonarqube.rest.plugins import SonarQubePlugins
 from sonarqube.rest.project_badges import SonarQubeProjectBadges
 from sonarqube.rest.project_tags import SonarQubeProjectTags
 from sonarqube.rest.project_analyses import SonarQubeProjectAnalyses
 from sonarqube.rest.server import SonarQubeServer
 from sonarqube.rest.user_tokens import SonarQubeUserTokens
-from sonarqube.rest.webhooks import SonarQubeWebhooks
-from sonarqube.rest.webservices import SonarQubeWebservices
-from sonarqube.rest.system import SonarQubeSystem
-from sonarqube.rest.alm_integrations import SonarQubeAlmIntegrations
-from sonarqube.rest.alm_settings import SonarQubeAlmSettings
-from sonarqube.rest.analysis_cache import SonarQubeAnalysisCache
 from sonarqube.rest.monitoring import SonarQubeMonitoring
 from sonarqube.rest.project_dump import SonarQubeProjectDump
-from sonarqube.rest.applications import SonarQubeApplications
 from sonarqube.rest.audit_logs import SonarQubeAuditLogs
 from sonarqube.rest.editions import SonarQubeEditions
 from sonarqube.rest.views import SonarQubeViews
-from sonarqube.rest.project_pull_requests import SonarQubeProjectPullRequests
 
 
 class SonarQubeClient:
@@ -134,33 +118,6 @@ class SonarQubeClient:
         return SonarQubeIssues(api=self)
 
     @property
-    def notifications(self):
-        """
-        SonarQube notifications Operations
-
-        :return:
-        """
-        return SonarQubeNotifications(api=self)
-
-    @property
-    def project_links(self):
-        """
-        SonarQube project links Operations
-
-        :return:
-        """
-        return SonarQubeProjectLinks(api=self)
-
-    @property
-    def permissions(self):
-        """
-        SonarQube permissions Operations
-
-        :return:
-        """
-        return SonarQubePermissions(api=self)
-
-    @property
     def ce(self):
         """
         SonarQube ce Operations
@@ -186,15 +143,6 @@ class SonarQubeClient:
         :return:
         """
         return SonarQubeQualityGates(api=self)
-
-    @property
-    def components(self):
-        """
-        SonarQube components Operations
-
-        :return:
-        """
-        return SonarQubeComponents(api=self)
 
     @property
     def rules(self):
@@ -233,24 +181,6 @@ class SonarQubeClient:
         return SonarQubeMetrics(api=self)
 
     @property
-    def hotspots(self):
-        """
-        Security Hotspots
-
-        :return:
-        """
-        return SonarQubeHotspots(api=self)
-
-    @property
-    def new_code_periods(self):
-        """
-        SonarQube new code periods Operations
-
-        :return:
-        """
-        return SonarQubeNewcodeperiods(api=self)
-
-    @property
     def settings(self):
         """
         SonarQube settings Operations
@@ -258,15 +188,6 @@ class SonarQubeClient:
         :return:
         """
         return SonarQubeSettings(api=self)
-
-    @property
-    def sources(self):
-        """
-        SonarQube sources Operations
-
-        :return:
-        """
-        return SonarQubeSources(api=self)
 
     @property
     def auth(self):
@@ -341,66 +262,6 @@ class SonarQubeClient:
         return SonarQubeUserTokens(api=self)
 
     @property
-    def webhooks(self):
-        """
-        SonarQube webhooks Operations
-
-        :return:
-        """
-        return SonarQubeWebhooks(api=self)
-
-    @property
-    def webservices(self):
-        """
-        SonarQube webservices Operations
-
-        :return:
-        """
-        return SonarQubeWebservices(api=self)
-
-    @property
-    def system(self):
-        """
-        SonarQube system Operations
-
-        :return:
-        """
-        return SonarQubeSystem(api=self)
-
-    @property
-    def plugins(self):
-        """
-        SonarQube plugins Operations
-
-        :return:
-        """
-        return SonarQubePlugins(api=self)
-
-    @property
-    def alm_integrations(self):
-        """
-        ALM Integrations
-
-        """
-        return SonarQubeAlmIntegrations(api=self)
-
-    @property
-    def alm_settings(self):
-        """
-        ALM settings
-
-        """
-        return SonarQubeAlmSettings(api=self)
-
-    @property
-    def analysis_cache(self):
-        """
-        Analysis cache
-
-        """
-        return SonarQubeAnalysisCache(api=self)
-
-    @property
     def monitoring(self):
         return SonarQubeMonitoring(api=self)
 
@@ -412,15 +273,6 @@ class SonarQubeClient:
         :return:
         """
         return SonarQubeProjectDump(api=self)
-
-    @property
-    def applications(self):
-        """
-        SonarQube applications Operations
-
-        :return:
-        """
-        return SonarQubeApplications(api=self)
 
     @property
     def audit_logs(self):
@@ -437,15 +289,6 @@ class SonarQubeClient:
 
         """
         return SonarQubeEditions(api=self)
-
-    @property
-    def project_pull_requests(self):
-        """
-        SonarQube project pull requests Operations
-
-        :return:
-        """
-        return SonarQubeProjectPullRequests(api=self)
 
     @property
     def views(self):
