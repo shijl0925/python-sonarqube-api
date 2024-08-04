3719e34c65581fcb5555fba6f5aeab1074ad8a20
shijl0925 <1213970695@qq.com>
2024-08-04 09:59:43 +0800

    add pytest case

--- tests/test_project_api.py
+++ tests/test_project_api.py
@@ -13,3 +13,23 @@ def test_search_projects(sonar_client):
     projects = [item["key"] for item in resp.get("components", [])]
 
     assert len(projects) >= 0
+
+def test_create_project(sonar_client):
+    organization = "williamsedmond90"
+    name = "sonar-java"
+    project_key = f"{organization}_sonar_java"
+    visibility = "public"
+    mainBranch = "master"
+
+    resp = sonar_client.projects.search_projects(projects=project_key, organization=organization)
+    components = resp.get("components", [])
+    if components:
+        return
+
+    sonar_client.projects.create_project(
+        project=project_key,
+        name=name,
+        organization=organization,
+        visibility=visibility,
+        mainBranch=mainBranch
+    )
